6ce0bad3c13c93b0d788d672e6af89ba655dbeb4
高境杉 <1359287622@qq.com>
2020-05-02 19:58:45 +0800

    王者荣耀筛选器

---
+++ 07_王者荣耀筛选器/pages/index/index.js
@@ -0,0 +1,26 @@
+Page({
+    data: {
+        arr: [],
+        nowType: '战士'
+    },
+    // 生命周期函数，就是Page的一生
+    // 当页面就绪之后
+    onReady() {
+        // 发出Ajax
+        wx.request({
+            url: 'http://www.aiqianduan.com:56506/wzry',
+            success: (data) => {
+                console.log(data.data);
+                this.setData({
+                    arr: data.data
+                })
+            }
+        })
+    },
+    // 改变筛选事件
+    changeOption(e) {
+        this.setData({
+            nowType: e.detail.value
+        })
+    }
+})
