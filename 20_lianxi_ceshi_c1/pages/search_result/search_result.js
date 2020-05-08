1663e8316246f1f1e4400a472eb4aef972cc113a
高境杉 <1359287622@qq.com>
2020-05-08 08:45:44 +0800

    二手车筛选

---
+++ 20_lianxi_ceshi_c1/pages/search_result/search_result.js
@@ -0,0 +1,58 @@
+// 全局的分页
+var page = 1;
+
+Page({
+    data: {
+        windowHeight: 0,
+        results: [],
+        show: false
+    },
+    onReady() {
+        wx.getSystemInfo({
+                success: (res) => {
+                    this.setData({
+                        windowHeight: res.windowHeight
+                    })
+                }
+            }),
+            // Ajax
+            wx.request({
+                'url': 'http://www.aiqianduan.com:7897/cars?page=' + page,
+                success: (data) => {
+                    this.setData({
+                        results: data.data.results
+                    })
+
+                    console.log(data.data.results);
+                }
+            })
+    },
+    scrolltolower() {
+        page++;
+        // Ajax
+        wx.request({
+            'url': 'http://www.aiqianduan.com:7897/cars?page=' + page,
+            success: (data) => {
+                wx.showLoading({
+                    title: '加载中'
+                })
+                this.setData({
+                    results: [...this.data.results, ...data.data.results]
+                })
+                wx.hideLoading();
+                console.log(data.data.results);
+            }
+        })
+    },
+    showPopup() {
+        this.setData({
+            show: true
+        });
+    },
+
+    onClose() {
+        this.setData({
+            show: false
+        });
+    }
+})
