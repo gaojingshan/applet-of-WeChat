aa75b57e524c0915097383cc22b58088f456c71c
高境杉 <1359287622@qq.com>
2020-09-13 15:19:24 +0800

    王者荣耀

--- 30_/pages/index/index.js
+++ 30_/pages/index/index.js
@@ -1,5 +1,29 @@
 //index.js
 
 Page({
-    
+    data: {
+        // Ajax获取的数据
+        arr: [],
+        // 英雄类别
+        hero: ['射手', '法师', '坦克', '辅助', '战士', '刺客'],
+        // 现在选中的类别
+        nowType: '法师'
+    },
+    onReady() {
+        wx.request({
+            url: 'http://www.aiqianduan.com:56506/wzry',
+            success: (data) => {
+                console.log(data.data);
+                this.setData({
+                    arr: data.data
+                })
+            }
+        })
+    },
+    radioHan(e) {
+        this.setData({
+            nowType: e.detail.value
+        })
+
+    }
 })
